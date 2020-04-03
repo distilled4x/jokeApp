a85d02a03ced4b746a35b191f06d993df76b68d9
distilled4x <Matt Huck>
2020-04-02 18:57:49 -0700

    first commit

---
+++ src/test/java/mhuck/udemyspring5/joke/jokeapp/JokeappApplicationTests.java
@@ -0,0 +1,13 @@
+package mhuck.udemyspring5.joke.jokeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokeappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
